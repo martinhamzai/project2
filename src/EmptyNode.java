dc2d101a031001f31e2247c25712985e009cf85b
Martin Hamzai <martinhamzai@outlook.com>
2025-03-21 14:11:10 -0400

    changes

--- src/EmptyNode.java
+++ src/EmptyNode.java
@@ -1,4 +1,10 @@
-
+/**
+ * EmptyNode class that implements the QuadNode interface. The QuadTree points
+ * to one instance of this class whenever an EmptyNode is needed to save space.
+ * 
+ * @author Martin Hamzai and Richmond Southall
+ * @version 2025-03-22
+ */
 public class EmptyNode implements QuadNode {
 
     private static EmptyNode flyweight = null;
@@ -8,6 +14,12 @@ public class EmptyNode implements QuadNode {
     }
 
 
+    /**
+     * Returns the instantiated EmptyNode or creates one if it has not already
+     * been instantiated.
+     * 
+     * @return the EmptyNode
+     */
     public static EmptyNode getInstance() {
         if (flyweight == null) {
             flyweight = new EmptyNode();
@@ -16,6 +28,10 @@ public class EmptyNode implements QuadNode {
     }
 
 
+    /**
+     * Creates a new LeafNode if the QuadTree lands on the EmptyNode for
+     * insertion.
+     */
     public QuadNode insert(
         KVPair<String, Point> pair,
         int x,
@@ -26,6 +42,9 @@ public class EmptyNode implements QuadNode {
     }
 
 
+    /**
+     * Prints out the information regarding the EmptyNode.
+     */
     public int dump(int depth, int x, int y, int size) {
         for (int i = 0; i < depth; i++) {
             System.out.print("  ");
@@ -36,12 +55,16 @@ public class EmptyNode implements QuadNode {
     }
 
 
-    @Override
-    public
-        QuadNode
-        remove(QuadNode node, Point p, int x, int y, int width, int height)
-    {
-        // TODO Auto-generated method stub
+    /**
+     * Redundant method inherited from the QuadNode interface.
+     */
+    public QuadNode remove(
+        QuadNode node,
+        Point p,
+        int x,
+        int y,
+        int width,
+        int height) {
         return null;
     }
 
---
+++ src/EmptyNodeTest.java
@@ -0,0 +1,45 @@
+import student.TestCase;
+
+/**
+ * This class tests the EmptyNode class so that the member methods work properly
+ * and that the expected behavior occurs.
+ * 
+ * @author Martin Hamzai and Richmond Southall
+ * 
+ * @version 03-22-2025
+ */
+public class EmptyNodeTest extends TestCase {
+    
+    private EmptyNode en;
+    
+    /**
+     * Initializes an EmptyNode object to be used for the tests.
+     */
+    public void setUp() {
+        en = EmptyNode.getInstance();
+    }
+    
+    /**
+     * Tests the insert method.
+     */
+    public void testInsert() {
+        assertTrue(en.insert(null, 0, 0, 0, 0) instanceof LeafNode);
+    }
+    
+    /**
+     * Tests the dump method.
+     */
+    public void testDump() {
+        int res = en.dump(1, 0, 0, 0);
+        assertTrue(res == 1);
+        assertEquals(systemOut().getHistory(), "  Node at 0, 0, 0: Empty\n");
+    }
+    
+    /**
+     * Tests the remove method.
+     */
+    public void testRemove() {
+        assertTrue(en.remove(en, null, 0, 0, 0, 0) == null);
+    }
+
+}
