03c8936160e2ef2b1e9fe99c6a08f4bcbcef78ad
richmonds2024S <richmonds@vt.edu>
2025-03-21 10:06:57 -0400

    Update InternalNode.java
    internal stuff

--- src/InternalNode.java
+++ src/InternalNode.java
@@ -1,4 +1,32 @@
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author 18046
+ *  @version Mar 21, 2025
+ */
+public class InternalNode<K extends Comparable<K>, E>  implements QuadNode{
 
-public class InternalNode {
-
+    public static EmptyNode flyweight = null;
+    
+    QuadNode nw, ne, sw, se;
+    int x, y, width, height;
+    
+    public InternalNode(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.nw = this.ne = this.sw = this.se = EmptyNode.getInstance();
+    }
+    
+    
+    @Override
+    public boolean insert(KVPair<String, Point> pair, int xMin, int xMax, int yMin, int yMax)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
 }
